816b23db19c4bf3ed9051902dbf7e862656e7abf
mark-summerfield <mark@qtrac.eu>
2024-11-15 09:33:26 +0000

    m

--- st.sh
+++ st.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 clc -s
 wc pcw.tdb
-echo no py app to check
-# unrecognized.py -q
-# python3 -m flake8 --ignore=W504,W503,E261,E303 .
-# python3 -m vulture . | grep -v 60%.confidence
-echo no go app to check
+unrecognized.py -q
+python3 -m flake8 --ignore=W504,W503,E261,E303 .
+python3 -m vulture . | grep -v 60%.confidence
 # go mod tidy
 # go fmt .
 # staticcheck .

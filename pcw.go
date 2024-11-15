d524adebb0674181454dc12ce8a246cb260add00
mark-summerfield <mark@qtrac.eu>
2024-11-15 10:41:35 +0000

    m

---
+++ pcw.go
@@ -0,0 +1,94 @@
+// Copyright © 2024 Mark Summerfield. All rights reserved.
+
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+
+	"github.com/mark-summerfield/tdb"
+	"github.com/otiai10/copy"
+)
+
+func main() {
+	root, www := preparePaths()
+	pcw := readDatabase(filepath.Join(root, "pcw.tdb"))
+	fmt.Println(root, www)
+	fmt.Println(pcw)
+	/* TODO create
+	- verify foreign keys & report any discrepencies
+	- index.html
+	- a page per issue with articles details + cover screenshot + pdf
+		link
+	- articles.html article index: each article may have multiple
+		entries, e.g., by title, by language/title, etc.
+	*/
+}
+
+func preparePaths() (string, string) {
+	root := getRoot()
+	www := filepath.Join(root, "www")
+	_ = os.RemoveAll(www)
+	os.Mkdir("www", 0750)
+	CheckErr(copy.Copy(filepath.Join(root, "covers"),
+		filepath.Join(www, "covers")))
+	return root, www
+}
+
+func getRoot() string {
+	cwd, err := os.Getwd()
+	CheckErr(err)
+	return cwd
+
+}
+
+func readDatabase(filename string) *PcwDatabase {
+	raw, err := os.ReadFile(filename)
+	CheckErr(err)
+	pcw := PcwDatabase{}
+	CheckErr(tdb.Unmarshal(raw, &pcw))
+	return &pcw
+}
+
+func CheckErr(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+type PcwDatabase struct {
+	About     []About
+	Kinds     []Kind
+	Languages []Language
+	Volumes   []Volume
+	Articles  []Article
+}
+
+type About struct{ About string }
+
+type Kind struct {
+	Kid  string
+	Name string
+}
+
+type Language struct {
+	Lid  string
+	Name string
+}
+
+type Volume struct {
+	Vid     string
+	Volnum  string
+	Pdflink string
+	Notes   string
+}
+
+type Article struct {
+	Vid    string
+	Title  string
+	Kid    string
+	Lid    string
+	Author string
+}
--- st.sh
+++ st.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 clc -s
 wc pcw.tdb
-unrecognized.py -q
-python3 -m flake8 --ignore=W504,W503,E261,E303,E402 .
-python3 -m vulture . | grep -v 60%.confidence
+# unrecognized.py -q
+# python3 -m flake8 --ignore=W504,W503,E261,E303,E402 .
+# python3 -m vulture . | grep -v 60%.confidence
+go mod tidy
+go fmt .
+staticcheck .
+go vet .
+golangci-lint run
 git st

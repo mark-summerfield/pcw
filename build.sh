6545d0c6b38aa9def06b5a216383e1cf499d2c59
mark-summerfield <mark@qtrac.eu>
2024-11-15 07:48:22 +0000

    m

--- build.sh
+++
@@ -1,2 +0,0 @@
-go build -ldflags "-s -w" -o pcw .
-strip pcw
--- st.sh
+++ st.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 clc -s
-go mod tidy
-go fmt .
-staticcheck .
-go vet .
-golangci-lint run
+echo no py app to check
+# unrecognized.py -q
+# python3 -m flake8 --ignore=W504,W503,E261,E303 .
+# python3 -m vulture . | grep -v 60%.confidence
+echo no go app to check
+# go mod tidy
+# go fmt .
+# staticcheck .
+# go vet .
+# golangci-lint run
 git st
